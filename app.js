8d2633e7eb2889c9eefa91135483c8c925149fe1
viveks221 <viveksingh6208@gmail.com>
2024-01-06 09:32:20 +0530

    added parcel bundler

---
+++ app.js
@@ -0,0 +1,43 @@
+import React from "react";
+import  ReactDOM  from "react-dom/client";
+
+// Manipulate the HTML DOM using Javscript
+// const heading = document.createElement("h1");
+// heading.innerHTML = "Namaste Everyone";
+// const root = document.getElementById("root");
+// root.appendChild(heading);
+
+// Manipulate the HTML DOM using React
+
+// Create nested React Elements
+const heading = React.createElement(
+    "h1",
+    {
+      id: "title",
+      style: {
+        background:"red",
+      },
+      className:"title"
+    },
+    "heading"
+  );
+  const heading1 = React.createElement(
+    "h1",
+    {
+      id: "title",
+    },
+    "heading1"
+  );
+  
+  const container = React.createElement(
+    "div",
+    {
+      id: "container",
+    },
+    [heading, heading1]
+  );
+  
+  // create root using createRoot
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  // passing react element inside root
+  root.render(container);
